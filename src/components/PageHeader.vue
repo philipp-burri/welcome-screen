46fad83c1febbc0de819831c2e1dfb37be2ef677
philipp-burri <burri.philipp13@gmail.com>
2024-08-27 16:25:30 +0200

    currentDate

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -3,13 +3,16 @@ import { ref } from 'vue';
 
 const cardTitle = ref('Welcome to Opportunity')
 
-let date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
 let options = { /* weekday: 'long',  */ year: 'numeric', month: 'numeric', day: 'numeric' };
+const currentDate = ref(new Date().toLocaleString("de-ch", options))
+
+let date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+
 </script>
 
 <template>
     <div class="cardTitle">{{ cardTitle }}</div>
-    <div class="date">{{ new Date().toLocaleString("de-ch", options) }}</div>
+    <div class="date">{{ currentDate }}</div>
 </template>
 
 <style scoped>
